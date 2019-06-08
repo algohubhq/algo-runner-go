7883f1cd27bb25d575827ebe1e716aec8a3d35e3
Mike Cowgill <mike@algohub.com>
2019-06-08 10:25:08 -0700

    Fixed metrics not showing
    Former-commit-id: a580d8060ee09be3c526d5d7d6c7a4bf0d7c8836

--- metrics.go
+++ metrics.go
@@ -4,37 +4,36 @@ import (
 	"fmt"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 // Global metrics variables
 var (
-	endpointLabel = fmt.Sprintf("%s_%s", config.EndpointOwnerUserName, config.EndpointName)
-	algoLabel     = fmt.Sprintf("%s_%s", config.AlgoOwnerUserName, config.AlgoName)
+	endpointLabel string
+	algoLabel     string
 
-	runnerRuntimeHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	runnerRuntimeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "algorunner_run_duration_seconds",
 		Help:    "The complete message processing duration in seconds",
 		Buckets: []float64{0.005, 0.05, 0.25, 1},
 	}, []string{"endpoint", "algo", "status"})
 
-	algoRuntimeHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
+	algoRuntimeHistogram = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "algorunner_algo_duration_seconds",
 		Help:    "The algo run duration in seconds",
 		Buckets: []float64{0.005, 0.05, 0.25, 1, 2.5, 5, 7.5, 10, 20, 30},
 	}, []string{"endpoint", "algo", "status"})
 
-	bytesProcessedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+	bytesProcessedCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "algorunner_bytes_processed_total",
 		Help: "The total number of bytes processed by the runner",
 	}, []string{"endpoint", "algo", "status"})
 
-	algoErrorCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+	algoErrorCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "algorunner_algo_error_total",
 		Help: "The total number of errors from the algo",
 	}, []string{"endpoint", "algo"})
 
-	runnerErrorCounter = promauto.NewCounterVec(prometheus.CounterOpts{
+	runnerErrorCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "algorunner_runner_error_total",
 		Help: "The total number of errors from the runner",
 	}, []string{"endpoint", "algo"})
@@ -42,10 +41,13 @@ var (
 
 func registerMetrics() {
 
-	prometheus.Register(runnerRuntimeHistogram)
-	prometheus.Register(algoRuntimeHistogram)
-	prometheus.Register(bytesProcessedCounter)
-	prometheus.Register(algoErrorCounter)
-	prometheus.Register(runnerErrorCounter)
+	endpointLabel = fmt.Sprintf("%s_%s", config.EndpointOwnerUserName, config.EndpointName)
+	algoLabel = fmt.Sprintf("%s_%s", config.AlgoOwnerUserName, config.AlgoName)
+
+	prometheus.MustRegister(runnerRuntimeHistogram)
+	prometheus.MustRegister(algoRuntimeHistogram)
+	prometheus.MustRegister(bytesProcessedCounter)
+	prometheus.MustRegister(algoErrorCounter)
+	prometheus.MustRegister(runnerErrorCounter)
 
 }
